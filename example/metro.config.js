45221aeb5bfec4318ba57bbb94bac4576a939b29
Sabarimalai  Sevakarkal <sabarimalaisevakarkalofficial@gmail.com>
2026-06-06 19:46:55 +0530

    chore: fix Node 18 compatibility and watchFolders in Metro config

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,4 +1,13 @@
+if (!Array.prototype.toReversed) {
+  Array.prototype.toReversed = function() {
+    return [...this].reverse();
+  };
+}
+
+const path = require('path');
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+
+const root = path.resolve(__dirname, '..');
 
 /**
  * Metro configuration
@@ -6,6 +15,14 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  watchFolders: [root],
+  resolver: {
+    nodeModulesPaths: [
+      path.resolve(__dirname, 'node_modules'),
+      path.resolve(root, 'node_modules'),
+    ],
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
---
+++ example/polyfill.js
@@ -0,0 +1,13 @@
+const util = require('util');
+
+if (!util.styleText) {
+  util.styleText = function(format, text) {
+    return text;
+  };
+}
+
+if (!Array.prototype.toReversed) {
+  Array.prototype.toReversed = function() {
+    return [...this].reverse();
+  };
+}
